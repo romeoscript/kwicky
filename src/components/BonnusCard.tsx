a12b06553ce18695cf72af785712e3e3e885edb6
romeoscript <romeobourne211@gmail.com>
2024-01-22 06:37:33 +0100

    imported special treat

--- src/components/BonnusCard.tsx
+++ src/components/BonnusCard.tsx
@@ -1,34 +1,29 @@
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
-   
-  export function CardDefault() {
+import React from 'react'
+
+type BonnusCardProps = {
+    imageUrl: string;
+    title: string;
+    description: string;
+    height?: string; 
+};
+
+const BonnusCard: React.FC<BonnusCardProps> = ({ imageUrl, title, description, height }) => {
+    const cardStyle: React.CSSProperties = {
+        height: height || '300px', // Default width is 24rem (w-96), but it can be overridden by props
+    };
+
     return (
-      <Card className="mt-6 w-96">
-        <CardHeader color="blue-gray" className="relative h-56">
-          <img
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-            alt="card-image"
-          />
-        </CardHeader>
-        <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            UI/UX Review Check
-          </Typography>
-          <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Button>Read More</Button>
-        </CardFooter>
-      </Card>
-    );
-  }
+        <div className="card card-compact bg-base-100 shadow-xl mb-[1rem] max-w-[500px]" style={cardStyle}>
+            <figure><img src={imageUrl} className='w-full object-cover' alt={title} /></figure>
+            <div className="card-body bg-[#01183C] rounded-b-md">
+                <h2 className="card-title">{title}</h2>
+                <p>{description}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BonnusCard
--- src/components/Special.tsx
+++ src/components/Special.tsx
@@ -1,31 +1,49 @@
-import React from 'react'
-import bag from '../assets/bag.png'
+
+
 import watch from '../assets/watch.png'
+import bag from '../assets/bag.png'
+import BonnusCard from './BonnusCard'
 
 const Special = () => {
     return (
-        <div className='m-[1rem]'>
+        <div className='m-[1rem] p-[2rem]'>
             <h2>Valentine Sales With </h2>
             <p>10% Discount</p>
 
-            <div className='flex gap-4'>
+            <div className='grid md:grid-cols-2 gap-4 my-[2rem] '>
                 <aside>
-                    <div>
-                        <img src={bag} alt="" />
-                    </div>
-                    <div>
-                        <img src={watch} alt="" />
-                    </div>
+                    <BonnusCard
+                        imageUrl={bag}
+                        title="Shoes!"
+                        description="If a dog chews shoes whose shoes does he choose?"
+                     
+                    />
+                    <BonnusCard
+                        imageUrl={watch}
+                        title="Shoes!"
+                        description="If a dog chews shoes whose shoes does he choose?"
+                        height="450px" // You can set custom width like this
+                    />
+
                 </aside>
                 <aside>
-                    <div>
-                        <img src={bag} alt="" />
-                    </div>
-                    <div>
-                        <img src={watch} alt="" />
-                    </div>
+                
+                    <BonnusCard
+                        imageUrl={watch}
+                        title="Shoes!"
+                        description="If a dog chews shoes whose shoes does he choose?"
+                        height="450px" // You can set custom width like this
+                    />
+
+<BonnusCard
+                        imageUrl={bag}
+                        title="Shoes!"
+                        description="If a dog chews shoes whose shoes does he choose?"
+                     
+                    />
+
                 </aside>
-           
+
             </div>
         </div>
     )
